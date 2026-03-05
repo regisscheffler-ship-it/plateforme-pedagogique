7f9053f8275e3ffad37e0ed9bd28891fcea01137
Regis Scheffler <regisscheffler-ship-it@users.noreply.github.com>
2026-03-05 10:57:36 +0100

    Fix ALLOWED_HOSTS pour onrender.com

--- plateforme/settings.py
+++ plateforme/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '7eSBcMNOnQlXa3iZQ9az-CKw7PzdCTMmLlRVm
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# Accepte automatiquement tous les sous-domaines onrender.com
+ALLOWED_HOSTS += ['.onrender.com']
 
 
 # Application definition
