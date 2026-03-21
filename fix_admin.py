e6013bd52e670a0b1daf7611358cbdcd8fedf2f3
Regis Scheffler <regisscheffler-ship-it@users.noreply.github.com>
2026-03-21 11:54:32 +0100

    feat: consigne communication élève + marquage lu sur réponse

---
+++ fix_admin.py
@@ -0,0 +1,11 @@
+﻿from django.contrib.auth.models import User
+from core.models import ProfilUtilisateur
+user = User.objects.get(username='admin')
+profil, created = ProfilUtilisateur.objects.get_or_create(
+    user=user,
+    defaults={'type_utilisateur': 'professeur', 'compte_approuve': True}
+)
+profil.type_utilisateur = 'professeur'
+profil.compte_approuve = True
+profil.save()
+print('OK - Type:', profil.type_utilisateur)
